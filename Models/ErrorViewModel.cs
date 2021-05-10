aa970f5cd93fb16c1d90daddf8f347e6cd0fa84c
Matheus <78980755+MRLimcon@users.noreply.github.com>
2021-05-10 13:57:18 +0000

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cotacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
