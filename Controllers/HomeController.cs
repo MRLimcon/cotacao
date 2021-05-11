8b7dfe9e388e47b3ab596568bae43ba258384557
Matheus <78980755+MRLimcon@users.noreply.github.com>
2021-05-11 12:59:09 -0300

    Atualização com a página de banco de moedas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace cotacao.Controllers
             return View();
         }
 
+        public IActionResult Moedas()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
