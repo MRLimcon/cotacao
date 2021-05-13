00c87b3e665f575a09fc4338bc8e771680b5e103
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2021-05-13 11:08:51 -0300

    Add Index controllers and add new route

--- Controllers/InvestController.cs
+++ Controllers/InvestController.cs
@@ -5,35 +5,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using cotacao.Serializers;
+using cotacao.Context;
 using cotacao.Models;
 
 namespace cotacao.Controllers
 {
     public class InvestController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
         private readonly ILogger<InvestController> _logger;
 
-        public InvestController(ILogger<InvestController> logger)
+        public InvestController(ILogger<InvestController> logger, ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var invests = _context.Invest.ToList();
+            // var invests = _context.Invest.Update();
+            // var invests = _context.Invest.Remove();
+            // var invests = _context.SaveChanges();
+            return View(invests);
+        }
+        
+        public IActionResult New(int investId)
+        {
+            var invests = _context.Invest.Find(investId);
+            return View(invests);
         }
 
-        public IActionResult Create()
+        [HttpPost]
+        public IActionResult Create(InvestForm form)
+        {
+            // var invests = _context.Invest.Add();
+            return RedirectToAction("Index", "Invest");
+        }
+
+
+        public IActionResult Read(int investId)
         {
             return View();
         }
 
-        public IActionResult Read()
-        {
-            return View();
-        }
-
-        public IActionResult Update()
+        [HttpPut]
+        public IActionResult Update(int investId)
         {
             return View();
         }
---
+++ Serializers/InvestForm.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cotacao.Serializers
+{
+    public class InvestForm
+    {
+        public DateTime date;
+        public string coin;
+
+    }
+}
